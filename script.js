f9b63c72d8df06de601e7a1f50832b699baf58b8
Kunal Yadav <kunalyadav2003@gmail.com>
2022-06-26 22:23:59 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+var hindi = document.getElementById("hindi");
+var english = document.getElementById("english");
+var russian = document.getElementById("russian");
+var french = document.getElementById("french");
+
+hindi.addEventListener("click", function() {
+    alert("Namaste Duniya!")
+  });
+
+english.addEventListener("click", function() {
+    alert("Hello World!")
+  });
+
+russian.addEventListener("click", function() {
+    alert("Привет, мир!!")
+  });
+
+french.addEventListener("click", function() {
+    alert("Bonjour le monde!!")
+  });
